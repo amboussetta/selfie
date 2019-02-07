750fcb32560319133bb6c566506e49a2041936a0
Mohammed Amine BOUSSETTA <a.boussetta@Mohammeds-MacBook-Pro.local>
2019-02-07 09:57:13 +0000

    Assignment1

--- selfie.c
+++ selfie.c
@@ -2,13 +2,10 @@
 Copyright (c) 2015-2019, the Selfie Project authors. All rights reserved.
 Please see the AUTHORS file for details. Use of this source code is
 governed by a BSD license that can be found in the LICENSE file.
-
 Selfie is a project of the Computational Systems Group at the
 Department of Computer Sciences of the University of Salzburg
 in Austria. For further information and code please refer to:
-
 http://selfie.cs.uni-salzburg.at
-
 The Selfie Project provides an educational platform for teaching
 undergraduate and graduate students the design and implementation
 of programming languages and runtime systems. The focus is on the
@@ -16,9 +13,7 @@ construction of compilers, libraries, operating systems, and even
 virtual machine monitors. The common theme is to identify and
 resolve self-reference in systems code which is seen as the key
 challenge when teaching systems engineering, hence the name.
-
 Selfie is a self-contained 64-bit, 10-KLOC C implementation of:
-
 1. a self-compiling compiler called starc that compiles
    a tiny but still fast subset of C called C Star (C*) to
    a tiny and easy-to-teach subset of RISC-V called RISC-U,
@@ -33,12 +28,10 @@ Selfie is a self-contained 64-bit, 10-KLOC C implementation of:
    with non-zero exit codes,
 5. a simple SAT solver that reads CNF DIMACS files, and
 6. a tiny C* library called libcstar utilized by selfie.
-
 Selfie is implemented in a single (!) file and kept minimal for simplicity.
 There is also a simple in-memory linker, a RISC-U disassembler, a profiler,
 and a debugger with replay as well as minimal operating system support in
 the form of RISC-V system calls built into the emulator.
-
 C* is a tiny Turing-complete subset of C that includes dereferencing
 (the * operator) but excludes composite data types, bitwise and Boolean
 operators, and many other features. There are only unsigned 64-bit
@@ -50,19 +43,16 @@ arithmetics helping students better understand arithmetic operators.
 C* is supposed to be close to the minimum necessary for implementing
 a self-compiling, single-pass, recursive-descent compiler. C* can be
 taught in one to two weeks of classes depending on student background.
-
 The compiler can readily be extended to compile features missing in C*
 and to improve performance of the generated code. The compiler generates
 RISC-U executables in ELF format that are compatible with the official
 RISC-V toolchain. The mipster emulator can execute RISC-U executables
 loaded from file but also from memory immediately after code generation
 without going through the file system.
-
 RISC-U is a tiny Turing-complete subset of the RISC-V instruction set.
 It only features unsigned 64-bit integer arithmetic, double-word memory,
 and simple control-flow instructions but neither bitwise nor byte- and
 word-level instructions. RISC-U can be taught in one week of classes.
-
 The emulator implements minimal operating system support that is meant
 to be extended by students, first as part of the emulator, and then
 ported to run on top of it, similar to an actual operating system or
@@ -71,13 +61,11 @@ helps exposing the self-referential nature of that challenge. In fact,
 selfie goes one step further by implementing microkernel functionality
 as part of the emulator and a hypervisor that can run as part of the
 emulator as well as on top of it, all with the same code.
-
 The symbolic execution engine implements a simple yet sound and
 complete translation of RISC-U code to SMT-LIB formulae. The SAT
 solver implements a naive brute-force enumeration of all possible
 variable assignments. Both engine and solver facilitate teaching
 the absolute basics of SAT and SMT solving applied to real code.
-
 Selfie is the result of many years of teaching systems engineering.
 The design of the compiler is inspired by the Oberon compiler of
 Professor Niklaus Wirth from ETH Zurich. RISC-U is inspired by the
@@ -384,12 +372,14 @@ uint64_t SYM_LT           = 24; // <
 uint64_t SYM_LEQ          = 25; // <=
 uint64_t SYM_GT           = 26; // >
 uint64_t SYM_GEQ          = 27; // >=
+uint64_t SYM_SLL          = 28; // <<
+uint64_t SYM_SRL          = 29; // >>
 
 // symbols for bootstrapping
 
-uint64_t SYM_INT      = 28; // int
-uint64_t SYM_CHAR     = 29; // char
-uint64_t SYM_UNSIGNED = 30; // unsigned
+uint64_t SYM_INT      = 30; // int
+uint64_t SYM_CHAR     = 31; // char
+uint64_t SYM_UNSIGNED = 32; // unsigned
 
 uint64_t* SYMBOLS; // strings representing symbols
 
@@ -455,6 +445,10 @@ void init_scanner () {
   *(SYMBOLS + SYM_LEQ)          = (uint64_t) "<=";
   *(SYMBOLS + SYM_GT)           = (uint64_t) ">";
   *(SYMBOLS + SYM_GEQ)          = (uint64_t) ">=";
+//modified for sll and arl definition
+  *(SYMBOLS + SYM_SLL)          = (uint64_t) "<<";
+  *(SYMBOLS + SYM_SRL)          = (uint64_t) ">>";
+
 
   *(SYMBOLS + SYM_INT)      = (uint64_t) "int";
   *(SYMBOLS + SYM_CHAR)     = (uint64_t) "char";
@@ -589,6 +583,7 @@ uint64_t is_literal();
 uint64_t is_star_or_div_or_modulo();
 uint64_t is_plus_or_minus();
 uint64_t is_comparison();
+uint64_t is_sll_or_srl();
 
 uint64_t look_for_factor();
 uint64_t look_for_statement();
@@ -617,6 +612,7 @@ uint64_t compile_factor();
 uint64_t compile_term();
 uint64_t compile_simple_expression();
 uint64_t compile_expression();
+uint64_t compile_bitwise_expression();
 void     compile_while();
 void     compile_if();
 void     compile_return();
@@ -826,6 +822,11 @@ uint64_t F3_SD    = 3; // 011
 uint64_t F3_BEQ   = 0; // 000
 uint64_t F3_JALR  = 0; // 000
 uint64_t F3_ECALL = 0; // 000
+//modified for srl and sll
+uint64_t F3_SLL    = 1; // 001
+uint64_t F3_SRL    = 5; // 101
+
+
 
 // f7-codes
 uint64_t F7_ADD  = 0;  // 0000000
@@ -834,7 +835,9 @@ uint64_t F7_SUB  = 32; // 0100000
 uint64_t F7_DIVU = 1;  // 0000001
 uint64_t F7_REMU = 1;  // 0000001
 uint64_t F7_SLTU = 0;  // 0000000
-
+//modifeid for sll and srl
+uint64_t F7_SLL = 0;
+uint64_t F7_SRL = 0;
 // f12-codes (immediates)
 uint64_t F12_ECALL = 0; // 000000000000
 
@@ -878,6 +881,11 @@ void emit_mul(uint64_t rd, uint64_t rs1, uint64_t rs2);
 void emit_divu(uint64_t rd, uint64_t rs1, uint64_t rs2);
 void emit_remu(uint64_t rd, uint64_t rs1, uint64_t rs2);
 void emit_sltu(uint64_t rd, uint64_t rs1, uint64_t rs2);
+//modified for srl and sll
+void emit_sll(uint64_t rd, uint64_t rs1, uint64_t rs2);
+void emit_srl(uint64_t rd, uint64_t rs1, uint64_t rs2);
+
+
 
 void emit_ld(uint64_t rd, uint64_t rs1, uint64_t immediate);
 void emit_sd(uint64_t rs1, uint64_t immediate, uint64_t rs2);
@@ -1107,6 +1115,10 @@ void do_divu();
 void do_remu();
 
 void do_sltu();
+// modified for srl and sll
+void do_sll();
+void do_srl();
+//
 void zero_extend_sltu();
 
 void     print_ld();
@@ -1842,9 +1854,9 @@ char* store_character(char* s, uint64_t i, uint64_t c) {
 }
 
 char* string_alloc(uint64_t l) {
-	// allocates zeroed memory for a string of l characters
+  // allocates zeroed memory for a string of l characters
   // plus a null terminator aligned to machine word size
-	return (char*) zalloc(l + 1);
+  return (char*) zalloc(l + 1);
 }
 
 uint64_t string_length(char* s) {
@@ -2874,31 +2886,34 @@ void get_symbol() {
 
       } else if (character == CHAR_EXCLAMATION) {
         get_character();
-
         if (character == CHAR_EQUAL)
           get_character();
         else
           syntax_error_character(CHAR_EQUAL);
-
         symbol = SYM_NOTEQ;
-
       } else if (character == CHAR_LT) {
         get_character();
-
         if (character == CHAR_EQUAL) {
           get_character();
 
           symbol = SYM_LEQ;
+        } else if (character == CHAR_LT){
+          get_character();
+
+          symbol = SYM_SLL;//modified for sll and srl
         } else
           symbol = SYM_LT;
 
       } else if (character == CHAR_GT) {
         get_character();
-
         if (character == CHAR_EQUAL) {
           get_character();
 
           symbol = SYM_GEQ;
+        } else if (character == CHAR_GT) {
+          get_character();
+
+          symbol = SYM_SRL;
         } else
           symbol = SYM_GT;
 
@@ -2907,9 +2922,8 @@ void get_symbol() {
         print("found unknown character ");
         print_character(character);
         println();
-
         exit(EXITCODE_SCANNERERROR);
-      }
+        }
     }
 
     number_of_scanned_symbols = number_of_scanned_symbols + 1;
@@ -3133,6 +3147,18 @@ uint64_t is_star_or_div_or_modulo() {
   else
     return 0;
 }
+
+//modified for srl and sll 
+//to be added in the compile_bitwise_expression
+uint64_t is_sll_or_srl() {
+  if (symbol == SYM_SLL)
+    return 1;
+  else if (symbol == SYM_SRL)
+    return 1;
+  else
+    return 0;
+}
+
 
 uint64_t is_plus_or_minus() {
   if (symbol == SYM_MINUS)
@@ -3821,6 +3847,64 @@ uint64_t compile_term() {
   return ltype;
 }
 
+//modified for sll and srl adding
+
+uint64_t compile_bitwise_expression() {
+  uint64_t ltype;
+  uint64_t operator_symbol;
+  uint64_t rtype;
+
+  // assert: n = allocated_temporaries
+
+  ltype = compile_simple_expression();
+
+  // assert: allocated_temporaries == n + 1
+
+  // modified for sll and srl
+  // << or >> ?
+  while (is_sll_or_srl()) {
+    operator_symbol = symbol;
+
+    get_symbol();
+
+    rtype = compile_simple_expression();
+
+    // assert: allocated_temporaries == n + 2
+
+    if (operator_symbol == SYM_SLL) {
+      if (ltype == UINT64_T) {
+        if (rtype == UINT64_T){
+          emit_sll(previous_temporary(), previous_temporary(), current_temporary());
+        } else
+          syntax_error_message("(uint64_t) << (uint64_t*) is undefined");
+      } else
+        syntax_error_message("(uint64_t*) << (uint64_t*) is undefined");
+
+    } else if (operator_symbol == SYM_SRL) {
+      if (ltype == UINT64_T) {
+        if (rtype == UINT64_T) {
+          emit_srl(previous_temporary(), previous_temporary(), current_temporary());
+        } else 
+          syntax_error_message("(uint64_t) >> (uint64_t*) is undefined");
+      } else 
+        syntax_error_message("(uint64_t*) >> (uint64_t*) is undefined");
+    }
+    tfree(1);
+  }
+
+  // assert: allocated_temporaries == n + 1
+
+  return ltype;
+}
+
+
+
+
+
+
+
+
+
 uint64_t compile_simple_expression() {
   uint64_t ltype;
   uint64_t operator_symbol;
@@ -3899,8 +3983,9 @@ uint64_t compile_expression() {
   uint64_t rtype;
 
   // assert: n = allocated_temporaries
-
-  ltype = compile_simple_expression();
+  // modified for srl and sll
+  //changed to accomodate the changes in the grammer.md
+  ltype = compile_bitwise_expression();
 
   // assert: allocated_temporaries == n + 1
 
@@ -3909,8 +3994,8 @@ uint64_t compile_expression() {
     operator_symbol = symbol;
 
     get_symbol();
-
-    rtype = compile_simple_expression();
+    // modified for srl and sll
+    rtype = compile_bitwise_expression();
 
     // assert: allocated_temporaries == n + 2
 
@@ -5416,6 +5501,21 @@ void emit_add(uint64_t rd, uint64_t rs1, uint64_t rs2) {
   ic_add = ic_add + 1;
 }
 
+
+// modified for sll and srl
+void emit_sll(uint64_t rd, uint64_t rs1, uint64_t rs2) {
+  emit_instruction(encode_r_format(F7_SLL, rs2, rs1, F3_SLL, rd, OP_OP));
+
+  //ic_add = ic_add + 1;
+}
+
+void emit_srl(uint64_t rd, uint64_t rs1, uint64_t rs2) {
+  emit_instruction(encode_r_format(F7_SRL, rs2, rs1, F3_SRL, rd, OP_OP));
+
+  //ic_add = ic_add + 1;
+}
+// end of modification
+
 void emit_sub(uint64_t rd, uint64_t rs1, uint64_t rs2) {
   emit_instruction(encode_r_format(F7_SUB, rs2, rs1, F3_SUB, rd, OP_OP));
 
@@ -5433,6 +5533,7 @@ void emit_divu(uint64_t rd, uint64_t rs1, uint64_t rs2) {
 
   ic_divu = ic_divu + 1;
 }
+
 
 void emit_remu(uint64_t rd, uint64_t rs1, uint64_t rs2) {
   emit_instruction(encode_r_format(F7_REMU, rs2, rs1, F3_REMU, rd, OP_OP));
@@ -6738,6 +6839,31 @@ void do_add() {
   ic_add = ic_add + 1;
 }
 
+
+// modifeid for srl and sll
+void do_sll() {
+  if (rd != REG_ZR)
+    // semantics of add
+    *(registers + rd) = *(registers + rs1) << *(registers + rs2);
+
+  pc = pc + INSTRUCTIONSIZE;
+
+  //ic_add = ic_add + 1;
+}
+void do_srl() {
+  if (rd != REG_ZR)
+    // semantics of add
+    *(registers + rd) = *(registers + rs1) >> *(registers + rs2);
+
+  pc = pc + INSTRUCTIONSIZE;
+
+  //ic_add = ic_add + 1;
+}
+
+//
+
+
+
 void constrain_add_sub_mul_divu_remu_sltu(char* operator) {
   char* op1;
   char* op2;
@@ -7805,6 +7931,9 @@ void decode_execute() {
           do_divu();
 
         return;
+      }else if (funct7 == F7_SRL){// modified for srl and sll
+        do_srl();
+        return;
       }
     } else if (funct3 == F3_REMU) {
       if (funct7 == F7_REMU) {
@@ -7857,7 +7986,12 @@ void decode_execute() {
 
         return;
       }
+    }// Modified to accomodate the same number in function3 F3_DIVU=F3_SRL= 
+    else if (funct3 == F3_SLL){
+      do_sll();
+      return;
     }
+
   } else if (opcode == OP_BRANCH) {
     decode_b_format();
 
@@ -8618,12 +8752,10 @@ uint64_t up_load_string(uint64_t* context, char* s, uint64_t SP) {
 
 void up_load_arguments(uint64_t* context, uint64_t argc, uint64_t* argv) {
   /* upload arguments like a UNIX system
-
       SP
       |
       V
    | argc | argv[0] | ... | argv[n] | 0 | env[0] | ... | env[m] | 0 |
-
      with argc > 0, n == argc - 1, and m == 0 (that is, env is empty) */
   uint64_t SP;
   uint64_t* vargv;
